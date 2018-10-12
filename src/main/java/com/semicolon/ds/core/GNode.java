62689420d4030e1307481233e6a92c62460c74cc
Arunan Sugunakumar <arunans23@gmail.com>
2018-10-12 12:51:05 +0530

    create a Node class with basic variables

---
+++ src/main/java/com/semicolon/ds/core/GNode.java
@@ -0,0 +1,20 @@
+package com.semicolon.ds.core;
+
+public class GNode {
+
+    private String userName;
+    private String ipAddress;
+    private String port;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    public String getPort(){
+        return port;
+    }
+}

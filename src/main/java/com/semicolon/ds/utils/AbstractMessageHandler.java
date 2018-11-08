23672f7e8327c3185511cd73f778d79e129fa963
JMIsham <isham.14@cse.mrt.ac.lk>
2018-11-09 00:19:05 +0530

    refactoring

--- src/main/java/com/semicolon/ds/utils/AbstractMessageHandler.java
+++ src/main/java/com/semicolon/ds/utils/AbstractMessageHandler.java
@@ -1,11 +1,14 @@
 package com.semicolon.ds.utils;
 
 import com.semicolon.ds.comms.ChannelMessage;
-import com.semicolon.ds.core.MessageBroker;
 import com.semicolon.ds.core.RoutingTable;
+import com.semicolon.ds.core.TimeoutManager;
 
 import java.util.concurrent.BlockingQueue;
 
 interface AbstractMessageHandler {
-    void init (RoutingTable routingTable, BlockingQueue<ChannelMessage> channelOut);
+    void init (
+            RoutingTable routingTable,
+            BlockingQueue<ChannelMessage> channelOut,
+            TimeoutManager timeoutManager);
 }

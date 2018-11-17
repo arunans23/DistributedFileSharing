2e20c4002eacdcecbb1b77a7b0ded3b1c3abf0e0
thuva4 <tharma.thuva@gmail.com>
2018-11-17 11:47:52 +0530

    Modified the ftp server and client
    Signed-off-by: thuva4 <tharma.thuva@gmail.com>

--- src/main/java/com/semicolon/ds/comms/ftp/FTPClient.java
+++ src/main/java/com/semicolon/ds/comms/ftp/FTPClient.java
@@ -5,46 +5,21 @@ import java.net.Socket;
 
 public class FTPClient {
 
+    String fileName;
+
     public FTPClient() throws Exception {
 
         long start = System.currentTimeMillis();
         // localhost for testing
-        Socket sock = new Socket("127.0.0.1", 13267);
+        Socket serverSock = new Socket("127.0.0.1", 13267);
+
         System.out.println("Connecting...");
-        InputStream is = sock.getInputStream();
-        // receive file
-
-        DataOutputStream dOut = new DataOutputStream(sock.getOutputStream());
-        dOut.writeUTF("test.txt");
-        dOut.flush();
-        this.receiveFile(is);
-        long end = System.currentTimeMillis();
-        System.out.println(end - start);
-
-        sock.close();
+        Thread t = new Thread(new DataReceivingOperation(serverSock, "test.txt"));
+        t.start();
     }
 
+    public static void main(String[] args) throws Exception {
+        FTPClient ftpClient = new FTPClient();
 
-    public void receiveFile(InputStream is) throws Exception {
-        int filesize = 6022386;
-        int bytesRead;
-        int current = 0;
-        byte[] mybytearray = new byte[filesize];
-
-        FileOutputStream fos = new FileOutputStream("def");
-        BufferedOutputStream bos = new BufferedOutputStream(fos);
-        bytesRead = is.read(mybytearray, 0, mybytearray.length);
-        current = bytesRead;
-
-        do {
-            bytesRead = is.read(mybytearray, current,
-                    (mybytearray.length - current));
-            if (bytesRead >= 0)
-                current += bytesRead;
-        } while (bytesRead > -1);
-
-        bos.write(mybytearray, 0, current);
-        bos.flush();
-        bos.close();
     }
 }
--- src/main/java/com/semicolon/ds/comms/ftp/FTPServer.java
+++ src/main/java/com/semicolon/ds/comms/ftp/FTPServer.java
@@ -18,26 +18,14 @@ public class FTPServer {
             System.out.println("Waiting...");
 
             clientsocket = serverSocket.accept();
-            System.out.println("Accepted connection : " + clientsocket);
-            OutputStream os = clientsocket.getOutputStream();
-            InputStream is = clientsocket.getInputStream();
-            DataInputStream dIn = new DataInputStream(clientsocket.getInputStream());
-            String fileName = dIn.readUTF();
-            this.send(os, fileName);
-            clientsocket.close();
+            Thread t = new Thread(new DataSendingOperation(clientsocket));
+            t.start();
         }
 
     }
 
-    public void send(OutputStream os, String fileName) throws Exception {
-        // sendfile
-        File myFile = new File(fileName);
-        byte[] mybytearray = new byte[(int) myFile.length() + 1];
-        FileInputStream fis = new FileInputStream(myFile);
-        BufferedInputStream bis = new BufferedInputStream(fis);
-        bis.read(mybytearray, 0, mybytearray.length);
-        System.out.println("Sending...");
-        os.write(mybytearray, 0, mybytearray.length);
-        os.flush();
+    public static void main(String[] args) throws Exception {
+        FTPServer ftpServer = new FTPServer();
     }
+
 }

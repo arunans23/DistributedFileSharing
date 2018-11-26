52f9711cd0b940358b65f69fca23474675838328
kumar <inthira.14@cse.mrt.ac.lk>
2018-11-26 13:43:52 +0530

    added gui

---
+++ src/main/java/com/semicolon/ds/gui/Controller.java
@@ -0,0 +1,82 @@
+package com.semicolon.ds.gui;
+
+import com.semicolon.ds.core.GNode;
+import javafx.application.Platform;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.UUID;
+
+public class Controller implements Initializable {
+
+
+    public GNode node;
+
+    public Button buttonSearch;
+    public Button leaveButton;
+
+    public Label labelAddress;
+    public Label labelPort;
+
+
+    public TextArea areaAvailable;
+    public TextArea areaSearch;
+    public TextArea areaTable;
+
+    public TextField textSearch;
+
+    public void searchAction() {
+        String data = textSearch.getText();
+        areaSearch.setText(String.valueOf(node.doSearch(data)));
+
+    }
+
+    public void setData(String ipaddress, String availableData, String port) {
+        labelAddress.setText(ipaddress);
+        areaAvailable.setText(availableData);
+        labelPort.setText(port);
+
+    }
+
+    public void leaveAction() {
+
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Are you leaving ?", ButtonType.YES, ButtonType.NO);
+        alert.showAndWait();
+
+        if (alert.getResult() == ButtonType.YES) {
+            Platform.exit();
+            node.unRegister();
+            System.exit(0);
+        }
+
+
+    }
+
+    public void setRoutingTable(String table) {
+        areaTable.setText(table);
+
+    }
+
+    private String getRoutingTable() {
+        return "**RoutingTable**";
+    }
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        String uniqueID = UUID.randomUUID().toString();
+        try {
+            node = new GNode("node" + uniqueID);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        node.init();
+
+        this.setData(node.getIpAddress(), "Not yet", String.valueOf(node.getPort()));
+        this.setRoutingTable(this.getRoutingTable());
+
+    }
+}
---
+++ src/main/java/com/semicolon/ds/gui/Main.java
@@ -0,0 +1,39 @@
+package com.semicolon.ds.gui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    Stage window;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        window = primaryStage;
+//        FXMLLoader loader = new FXMLLoader();
+//        Parent root = loader.load(getClass().getResource("/first.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(
+                "/first.fxml"));
+        Parent root = loader.load();
+        Controller controller = loader.getController();
+        window.setOnCloseRequest(event -> {
+            event.consume();
+            controller.leaveAction();
+        });
+        primaryStage.setTitle("Distributed File Sharing");
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.show();
+
+
+    }
+
+
+}
